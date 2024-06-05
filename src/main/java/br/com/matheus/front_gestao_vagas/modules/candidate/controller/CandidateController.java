f046356a94c184e44ac8af994dc07ecc320b9329
Matheus Mello <matheusgmello1@gmail.com>
2024-06-05 11:56:38 -0300

    feat: pagina do perfil do candidato

--- src/main/java/br/com/matheus/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/matheus/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.matheus.front_gestao_vagas.modules.candidate.service.CandidateService;
@@ -12,22 +13,33 @@ import br.com.matheus.front_gestao_vagas.modules.candidate.service.CandidateServ
 @Controller
 @RequestMapping("/candidate")
 public class CandidateController {
-  
-  @Autowired
-  private CandidateService candidateService;
 
-  @GetMapping("/login")
-  public String login() {
-    return "candidate/login";
-  }
+    @Autowired
+    private CandidateService candidateService;
 
-  @PostMapping("/signIn")
-  public String signIn(RedirectAttributes redirectAttributes, String username, String password) {
-    if (username.equals("matheus")) {
-      return "candidate/profile";
-      
+    @GetMapping("/login")
+    public String login(){
+        return "candidate/login";
     }
-    redirectAttributes.addFlashAttribute("error_message", "Usuário ou senha incorretos");
-    return "redirect:/candidate/login";
-  }
+
+    @PostMapping("/signIn")
+    public String signIn(RedirectAttributes redirectAttributes, String username, String password){
+       
+        try{
+            var token = this.candidateService.login(username, password);
+    
+                return "candidate/profile";
+           
+            
+
+        }catch(HttpClientErrorException e){
+            redirectAttributes.addFlashAttribute("error_message", "Usuário ou senha incorretos");
+            return "redirect:/candidate/login";
+        }
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+      return "candidate/profile";
+    }
 }
