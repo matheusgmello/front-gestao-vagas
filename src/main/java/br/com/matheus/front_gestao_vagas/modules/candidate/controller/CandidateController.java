7b2c629e1d7254bb0f628e19a973c64ce5ff5fb5
Matheus <matheusgmello1@gmail.com>
2024-06-16 16:08:21 -0300

    feat: buscando vagas

--- src/main/java/br/com/matheus/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/matheus/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -84,8 +84,10 @@ public class CandidateController {
 
         try{
             if(filter != null) {
-                //model.addAttribute("jobs", filter)
-                this.findJobsService.execute(getToken(), filter);
+                
+                var jobs = this.findJobsService.execute(getToken(), filter);
+                model.addAttribute("jobs", jobs);
+
             }
         }catch(HttpClientErrorException e){
             return "redirect:/candidate/login";
--- src/main/java/br/com/matheus/front_gestao_vagas/modules/candidate/service/FindJobsService.java
+++ src/main/java/br/com/matheus/front_gestao_vagas/modules/candidate/service/FindJobsService.java
@@ -1,7 +1,10 @@
 package br.com.matheus.front_gestao_vagas.modules.candidate.service;
 
+import java.lang.reflect.Parameter;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -12,10 +15,12 @@ import org.springframework.web.client.HttpClientErrorException.Unauthorized;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.matheus.front_gestao_vagas.modules.candidate.dto.JobDTO;
+
 @Service
 public class FindJobsService {
 
-  public String execute(String token, String filter){
+  public List<JobDTO> execute(String token, String filter){
     RestTemplate rt = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
@@ -25,10 +30,11 @@ public class FindJobsService {
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/candidate/job")
                 .queryParam("filter", filter);
 
+        ParameterizedTypeReference<List<JobDTO>> responseType = new ParameterizedTypeReference<List<JobDTO>>() {};
+
         try{
-            var result = rt.exchange(builder.toUriString(), HttpMethod.GET, request, String.class);
-        System.out.println(result);
-        return result.getBody();
+            var result = rt.exchange(builder.toUriString(), HttpMethod.GET, request, responseType);
+            return result.getBody();
         }
         catch(Unauthorized ex){
             throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
